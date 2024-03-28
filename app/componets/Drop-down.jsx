0108d0206dd40b41f6f15cd77a99871fb1a11c94
FIloteus ngonyani <76960131+laughing-joker@users.noreply.github.com>
2024-03-28 21:27:49 +0300

    Made changes and added a component

---
+++ app/componets/Drop-down.jsx
@@ -0,0 +1,90 @@
+import {
+    Cloud,
+    CreditCard,
+    Github,
+    Keyboard,
+    LifeBuoy,
+    LogOut,
+    Mail,
+    MessageSquare,
+    Plus,
+    PlusCircle,
+    Settings,
+    User,
+    UserPlus,
+    Users,
+  } from "lucide-react"
+  
+  import { Button } from "@/components/ui/button"
+  import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  
+  export function DropdownMenuDemo(text, linkHref) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">{text}</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <Users className="mr-2 h-4 w-4" />
+              <span>Team</span>
+            </DropdownMenuItem>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <UserPlus className="mr-2 h-4 w-4" />
+                <span>Invite users</span>
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem>
+                    <Mail className="mr-2 h-4 w-4" />
+                    <span>Email</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <MessageSquare className="mr-2 h-4 w-4" />
+                    <span>Message</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <PlusCircle className="mr-2 h-4 w-4" />
+                    <span>More...</span>
+                  </DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Log out</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
+  }
+  
--- app/componets/Navbar.jsx
+++ app/componets/Navbar.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Link from 'next/link'
+import {DropdownMenuDemo} from './Drop-down';
+import { BiSolidGridAlt } from "react-icons/bi";
+import { BiSolidHome } from "react-icons/bi";
+import { BiSolidBell } from "react-icons/bi";
+import { BiSolidUser } from "react-icons/bi";
 export default function Navbar() {
   return (
     <nav>
-          <h1>Dojo Helpdesk</h1>
+          <div className='mt-2 flex  w-full justify-between border-b-4  border-blue-300  border-solid md:border-dotted'>
+            <div>
+              <BiSolidGridAlt style={{
+                width : '25px', height: '25px', cursor:'pointer',
+              }}/>
+            </div>
+            <div className='flex align-center relative' style={{ 
+            }}>
+            <BiSolidHome className='space-between' style={{
+                width : '25px', height: '25px', cursor:'pointer',
+              }}/>
+            <BiSolidBell style={{
+                width : '25px', height: '25px', cursor:'pointer',
+              }}/>
+            <BiSolidUser style={{
+                width : '25px', height: '25px', cursor:'pointer',
+              }}/>
+              <DropdownMenuDemo />
+            </div>
+
+          </div>
         </nav>
   )
 }
