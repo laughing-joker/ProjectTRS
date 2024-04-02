9bde9beb9b2fe114d34fefabe0684ec585051c6a
FIloteus ngonyani <76960131+laughing-joker@users.noreply.github.com>
2024-04-02 19:46:26 +0300

    added new pages and made changes to the home page

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import Navbar from '../componets/Navbar'
 export default function page() {
   return (
-    <div>
-      <Navbar/>
-      <div>  
+    <div className='bg-gray-100 w-full h-screen'>
+      <div>
+      <Navbar/>  
+    </div>
+    <div className='Main-content'>
+    <div className='flex flex-col justify-between'>
+    <div className=' bg-white shadow-md rounded-md h-screen'
+    style={{
+      width: '600px',
+      height: '300px',
+      marginTop: '28px',
+      marginLeft: '20px',
+      display: 'flex',
+      borderRadius: '5px',
+    }}><p className='text-center'>hello</p>
+    </div>
+    <div className=' bg-white shadow-md rounded-md h-screen'
+    style={{
+      width: '600px',
+      height: '190px',
+      marginTop: '18px',
+      marginLeft: '20px',
+      display: 'flex',
+      borderRadius: '5px',
+    }}><p className='text-center'>hello</p>
+    </div>
+    </div>
     </div>
     </div>
   )
